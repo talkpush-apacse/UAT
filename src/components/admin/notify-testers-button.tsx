6e544c0ed9fc1257dae67af06a58fb87e7f8d54e
Jolo Yu <jolo.yu@talkpush.com>
2026-03-15 23:36:39 +0800

    fix: ESLint no-unused-expressions in toggleOne

--- src/components/admin/notify-testers-button.tsx
+++ src/components/admin/notify-testers-button.tsx
@@ -43,7 +43,7 @@ export default function NotifyTestersButton({
   const toggleOne = (id: string) => {
     setSelectedIds((prev) => {
       const next = new Set(prev)
-      next.has(id) ? next.delete(id) : next.add(id)
+      if (next.has(id)) { next.delete(id) } else { next.add(id) }
       return next
     })
   }
